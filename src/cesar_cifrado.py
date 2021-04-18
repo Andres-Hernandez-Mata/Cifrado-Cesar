b588ba213ccfda24d6bfc19efcef759eaee97031
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-17 21:17:07 -0500

    Funcion encriptar
    Agregar funcion para encriptar frase

--- src/cesar_cifrado.py
+++ src/cesar_cifrado.py
@@ -5,3 +5,21 @@ Version: 1.1.0
 Python: 3.9.1
 Fecha: 17 Abril 2020
 """
+
+import os
+
+os.system("cls")
+
+def Encriptar(Frase):
+    FraseEnc = '' #str vacio
+    for letra in Frase: #recorro Frase letra por letra
+        encontrado = False
+        for x,y in abc.items():
+            if letra == x:
+                FraseEnc += y #fraseEnc.append(y)
+                encontrado = True
+        if not encontrado: #if encontrado == False
+                            # if encontrado != True
+            FraseEnc += letra
+    return FraseEnc
+
