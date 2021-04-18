488feb8ee0ff842a7219291f30ea91cc984c8243
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-17 20:59:20 -0500

    Agregar mensaje para cifrar
    Mensaje y clave

--- src/cifrado_cesar.py
+++ src/cifrado_cesar.py
@@ -6,3 +6,12 @@ Python: 3.9.1
 Fecha: 17 Abril 2020
 """
 
+message = input('Ingresa el mensaje: ')
+espacios = 1
+while espacios > 0:
+    clave = input('Ingresa tu palabra clave para cifrar: ')
+    espacios = clave.count(' ')
+    if clave.isalpha() == False:
+        espacios += 1
+key = len(clave)
+
