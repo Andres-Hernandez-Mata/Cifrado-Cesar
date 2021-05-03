878c6d08f746b6c0e3b966b800913e64d49853c7
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-03 01:32:33 -0500

    Funciones para cifrar y desencriptar
    Version 1.0.0

--- src/cesar.py
+++ src/cesar.py
@@ -22,3 +22,35 @@ if __name__ == "__main__":
     params = parser.parse_args()
 
 
+def cifrar(mensaje):
+    frase = ''
+    for letra in mensaje:
+        encontrado = False
+        for x,y in abc.items():
+            if letra == x:
+                frase += y
+                encontrado = True
+        if not encontrado:                            
+            frase += letra
+    return frase
+
+def desencriptar(mensaje):
+    frase = ''
+    for letra in mensaje:
+        encontrado = False
+        for x,y in abc.items():
+            if letra == y:
+                frase += x
+                encontrado = True
+        if not encontrado:
+            frase += letra
+    return frase
+
+abc = {
+    'A':'E', 'B':'F', 'C':'G', 'D':'H', 'E':'I',
+    'F':'J', 'G':'K', 'H':'L', 'I':'M', 'J':'N',
+    'K':'O', 'L':'P', 'M':'Q', 'N':'R', 'O':'S',
+    'P':'T', 'Q':'U', 'R':'V', 'S':'W', 'T':'X',
+    'U':'Y', 'V':'Z', 'W':'A', 'X':'B', 'Y':'C',
+    'Z':'D'
+}
